fafeb4643eda1693fe6b64ddf0aa9b7beaf50774
karadalex <karadalex@gmail.com>
2019-05-25 16:46:38 +0300

    ROS Connection

--- unity-raven2/Assets/Scripts/ROSConnection.cs
+++ unity-raven2/Assets/Scripts/ROSConnection.cs
@@ -1,20 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
+using RosSharp.RosBridgeClient;
+using std_msgs = RosSharp.RosBridgeClient.Messages.Standard;
 
 public class ROSConnection : MonoBehaviour
 {
+    RosSocket rosSocket;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // WebSocket Connection:
+        rosSocket = new RosSocket(new RosSharp.RosBridgeClient.Protocols.WebSocketNetProtocol("ws://localhost:9090"));
+
+        // Subscription:
+        string subscription_id = rosSocket.Subscribe<std_msgs.String>("/subscription_test", SubscriptionHandler);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Publication:
+        string publication_id = rosSocket.Advertise<std_msgs.String>("publication_test");
+        std_msgs.String message = new std_msgs.String();
+        message.data = "publication test message data";
+        rosSocket.Publish(publication_id, message);
     }
 
+    private static void SubscriptionHandler(std_msgs.String message)
+    {
+        Debug.Log((message).data);
+    }
+
+    void OnDestroy()
+    {
+        // Close ROS socket connection
+        rosSocket.Close();
+    }
     
 }

64adf69724c8b5ac812459d46bfe5feafe7a3806
karadalex <karadalex@gmail.com>
2019-05-26 23:32:45 +0300

    Move signals outside of sysCall_init and added table forms of arm signals

--- vrep-raven2/simulation.lua
+++ vrep-raven2/simulation.lua
@@ -8,26 +8,59 @@
 -- (the file can be opened in this editor with the popup menu over
 -- the file name)
 
+-- Set global variables (signals)
+rightArmHandles = {-1,-1,-1,-1,-1,-1}
+rightArmHandles[1] = sim.getObjectHandle('shoulder_R')
+rightArmHandles[2] = sim.getObjectHandle('elbow_R')
+rightArmHandles[3] = sim.getObjectHandle('insertion_R')
+rightArmHandles[4] = sim.getObjectHandle('tool_roll_R')
+rightArmHandles[5] = sim.getObjectHandle('wrist_joint_R')
+rightArmHandles[6] = sim.getObjectHandle('grasper_joint_1_R')
+sim.setStringSignal("rightArmHandles", sim.packTable(rightArmHandles))
+
+-- rightArmHandles in table form
+rightArmHandlesTable = {-1,-1,-1,-1,-1,-1}
+rightArmHandlesTable['shoulder_R'] = sim.getObjectHandle('shoulder_R')
+rightArmHandlesTable['elbow_R'] = sim.getObjectHandle('elbow_R')
+rightArmHandlesTable['insertion_R'] = sim.getObjectHandle('insertion_R')
+rightArmHandlesTable['tool_roll_R'] = sim.getObjectHandle('tool_roll_R')
+rightArmHandlesTable['wrist_joint_R'] = sim.getObjectHandle('wrist_joint_R')
+rightArmHandlesTable['grasper_joint_1_R'] = sim.getObjectHandle('grasper_joint_1_R')
+sim.setStringSignal("rightArmHandlesTable", sim.packTable(rightArmHandlesTable))
+
+leftArmHandles = {-1,-1,-1,-1,-1,-1}
+leftArmHandles[1] = sim.getObjectHandle('shoulder_L')
+leftArmHandles[2] = sim.getObjectHandle('elbow_L')
+leftArmHandles[3] = sim.getObjectHandle('insertion_L')
+leftArmHandles[4] = sim.getObjectHandle('tool_roll_L')
+leftArmHandles[5] = sim.getObjectHandle('wrist_joint_L')
+leftArmHandles[6] = sim.getObjectHandle('grasper_joint_1_L')
+sim.setStringSignal("leftArmHandles", sim.packTable(leftArmHandles))
+
+-- leftArmHandles in table form
+leftArmHandlesTable = {-1,-1,-1,-1,-1,-1}
+leftArmHandlesTable['shoulder_L'] = sim.getObjectHandle('shoulder_L')
+leftArmHandlesTable['elbow_L'] = sim.getObjectHandle('elbow_L')
+leftArmHandlesTable['insertion_L'] = sim.getObjectHandle('insertion_L')
+leftArmHandlesTable['tool_roll_L'] = sim.getObjectHandle('tool_roll_L')
+leftArmHandlesTable['wrist_joint_L'] = sim.getObjectHandle('wrist_joint_L')
+leftArmHandlesTable['grasper_joint_1_L'] = sim.getObjectHandle('grasper_joint_1_L')
+sim.setStringSignal("leftArmHandlesTable", sim.packTable(leftArmHandlesTable))
+
+jointNames = {
+    left = {
+        'shoulder_L', 'elbow_L', 'insertion_L', 'tool_roll_L', 'wrist_joint_L', 'grasper_joint_1_L'
+    },
+    right = {
+        'shoulder_R', 'elbow_R', 'insertion_R', 'tool_roll_R', 'wrist_joint_R', 'grasper_joint_1_R'
+    }
+}
+sim.setStringSignal("jointNames", sim.packTable(jointNames))
+
+
 function sysCall_init()
     -- Initialization code:
-    -- Set global variables (signals)
-    rightArmHandles = {-1,-1,-1,-1,-1,-1}
-    rightArmHandles[1] = sim.getObjectHandle('shoulder_R')
-    rightArmHandles[2] = sim.getObjectHandle('elbow_R')
-    rightArmHandles[3] = sim.getObjectHandle('insertion_R')
-    rightArmHandles[4] = sim.getObjectHandle('tool_roll_R')
-    rightArmHandles[5] = sim.getObjectHandle('wrist_joint_R')
-    rightArmHandles[6] = sim.getObjectHandle('grasper_joint_1_R')
-    sim.setStringSignal("rightArmHandles", sim.packTable(rightArmHandles))
-
-    leftArmHandles = {-1,-1,-1,-1,-1,-1}
-    leftArmHandles[1] = sim.getObjectHandle('shoulder_L')
-    leftArmHandles[2] = sim.getObjectHandle('elbow_L')
-    leftArmHandles[3] = sim.getObjectHandle('insertion_L')
-    leftArmHandles[4] = sim.getObjectHandle('tool_roll_L')
-    leftArmHandles[5] = sim.getObjectHandle('wrist_joint_L')
-    leftArmHandles[6] = sim.getObjectHandle('grasper_joint_1_L')
-    sim.setStringSignal("leftArmHandles", sim.packTable(leftArmHandles))
+    
     
 end
 
